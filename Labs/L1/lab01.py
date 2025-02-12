9594733f0fe457a722aaa271e120d17b20e15664
Mansur <mansur.r@outlook.com>
2025-02-12 18:47:17 +0530

    .

--- Labs/L1/lab01.py
+++ Labs/L1/lab01.py
@@ -1,3 +1,14 @@
-import pandas, sklearn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+# For splitting our data into 2 sets - one for training and one for testing 
+from sklearn.cross_validation import train_test_split
 
+# Create the NN model
+
+# Train the model on the training set
+
+# Test the model on the testing set
+
+# Save the model in the models directory
